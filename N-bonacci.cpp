6dfe04bbc308c6874a74fcd431e5a4e5b6781ff7
okc06 <okc06107@naver.com>
2019-10-02 17:42:20 +0900

    Update N-bonacci.cpp

--- N-bonacci.cpp
+++ N-bonacci.cpp
@@ -16,23 +16,5 @@ long long int calc_next_bonacci(long long int N, long long int* arr,long long in
 
 int main()
 {
-	/*초기세팅*/
-	long long int bonacci_arr[1000] = {0};
-	long long int result;
-	long long int N, k;
-	long long int l;
 
-	cin >> N;
-	cin >> k;
-
-	bonacci_arr[N - 1] = 1;
-
-	for (l = 0; l < (k - N); l++)
-	{
-		calc_next_bonacci(N, bonacci_arr, l);
-	}
-
-
-
-	cout << bonacci_arr[k-1];
 }
